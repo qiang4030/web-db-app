28b61d53d3926ba5dd33167c74b52ae841a61f14
qiang4030 <weiqiang4030@163.com>
2022-06-12 20:26:13 +0800

    updated

--- web/models/tutorial.model.js
+++ web/models/tutorial.model.js
@@ -30,6 +30,66 @@ class Tutorial {
       return err;
     }
   }
+
+  //  根据 id 查询记录
+  async findById() {
+    const sql = "select * from tutorials where id = ?";
+    const values = Object.values(this.obj);
+    try {
+      return await execSQL(sql, values);
+    } catch (err) {
+      return err;
+    }
+  }
+
+  // 查找已发布的所有记录
+  async findAllPublished() {
+    const sql = "select * from tutorials where published = 1";
+    try {
+      const result = await execSQL(sql);
+      return result;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  // 根据 id 修改记录
+  async updateOne() {
+    const sql =
+      "update tutorials set title = ?, description = ?, published = ? where id = ?";
+    const values = Object.values(this.obj);
+    values.push(Object.values(this.obj2)[0]);
+    console.log(values);
+    try {
+      const result = await execSQL(sql, values);
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  // 根据 id 删除记录
+  async deleteOne() {
+    const sql = "delete from tutorials where id = ?";
+    const values = Object.values(this.obj2);
+    try {
+      const result = await execSQL(sql, values);
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  // 删除所有记录
+  async deleteAll() {
+    const sql = "delete from tutorials where id";
+    try {
+      return await execSQL(sql);
+    } catch (err) {
+      return err;
+    }
+  }
 }
 
 module.exports = Tutorial;
