e96d64af13ade5f63b1abef8558e87a52c27d88d
qiang4030 <weiqiang4030@163.com>
2022-06-12 18:27:19 +0800

    added create route

--- web/index.js
+++ web/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const connection = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQL_HOST || "localhost",
@@ -13,6 +16,24 @@ const connection = mysql.createPool({
 app.get("/", (req, res) => {
   const sql = "select * from tutorials";
   connection.query(sql, (err, rows) => {
+    if (err) {
+      return res.json({
+        status: "failed",
+        msg: err,
+      });
+    }
+    res.json({
+      status: "success",
+      msg: rows,
+    });
+  });
+});
+
+app.post("/create", (req, res) => {
+  const sql =
+    "insert into tutorials (title,description,published) values (?,?,?)";
+  const values = Object.values(req.body);
+  connection.query(sql, values, (err, rows) => {
     if (err) {
       return res.json({
         status: "failed",
