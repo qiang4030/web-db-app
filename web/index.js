08cab2293665d805d1f605ab304224fba2edbe70
qiang4030 <weiqiang4030@163.com>
2022-06-12 13:24:56 +0800

    updated

--- web/index.js
+++ web/index.js
@@ -30,7 +30,7 @@ const tutorialRouter = require("./routes/tutorial.route");
 
 app.use("/tutorial", tutorialRouter);
 
-const PORT = process.env.PORT || "5000";
+const PORT = process.env.PORT || "3000";
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
